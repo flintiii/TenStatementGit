8109850e65ac6f24e4e8f09541df16a652a062dd
flintiii <flint@flint.com>
2023-06-13 15:06:17 -0400

    cleaned up repo

--- bin/3tsu.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash 
-# Tue 14 Feb 2023 01:56:12 PM EST 
-version="0.01"
-#
-git remote add origin git@github.com:flintiii/TenStatementGit.git
-git branch -M main
-git push -u origin main
